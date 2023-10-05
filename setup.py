faa5ea008caa5597ccad507349313ad1bbfa3440
twipiiti-benseman <twipiiti-benseman@kpmg.co.nz>
2023-10-05 17:19:12 +1300

    import bug toml and setup updates

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     description='Python pckage to simplify working with pyspark by using familiar pandas syntax.',
     python_requires='>=3.8.0, <3.10',
     setup_requires=[
-        'setuptools==65.4.1', 'dunamai==1.8.0', 'jinja2==3.0.3'
+        'setuptools==65.4.1', 'dunamai==1.8.0', 'jinja2==3.0.3', 'wheel'
     ]
 )
